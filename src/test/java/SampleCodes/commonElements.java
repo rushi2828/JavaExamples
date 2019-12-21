364ccddd36acfc1c62065f55c02c4b85340d043f
rushi2828 <rushi2828@gmail.com>
2019-12-21 15:52:47 +0530

    Small changes

--- src/test/java/SampleCodes/commonElements.java
+++ src/test/java/SampleCodes/commonElements.java
@@ -10,8 +10,8 @@ public class commonElements {
 		int[] i1 = {1,2,3,4,5,6,7,8};
 		int[] i2 = {5,6,7,8,9,10,11,12};
 		
-		HashSet<int[]> set1 = new HashSet<>(Arrays.asList(i1));
-		HashSet<int[]> set2 = new HashSet<>(Arrays.asList(i2));
+		HashSet<int[]> set1 = new HashSet<int[]>(Arrays.asList(i1));
+		HashSet<int[]> set2 = new HashSet<int[]>(Arrays.asList(i2));
 		
 		set1.retainAll(set2);
 		System.out.println(set1);
